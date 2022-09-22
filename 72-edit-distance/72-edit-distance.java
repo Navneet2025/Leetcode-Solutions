2bc745a86cf3b28e5244b88b6ee42409bf060f53
Navneet2025 <109101596+Navneet2025@users.noreply.github.com>
2022-09-22 19:22:17 +0530

    Time: 15 ms (10.99%), Space: 44.3 MB (80.84%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -3,7 +3,7 @@ class Solution {
         int m=word1.length();
         int n=word2.length();
         
-        int [][] dp=new int[m+1][n+1];
+        int dp[][]=new int[m+1][n+1];
         
         for(int i=0;i<=m;i++){
             dp[i][0]=i;
@@ -15,12 +15,12 @@ class Solution {
         
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
                     dp[i][j]=dp[i-1][j-1];
                 }
                 
                 else{
-                    dp[i][j]=1 + Math.min(Math.min(dp[i][j-1] , dp[i-1][j]) , dp[i-1][j-1]);
+                    dp[i][j]=1 + Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
                 }
             }
         }
