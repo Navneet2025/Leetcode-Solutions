7b81bd0f2223562ebab856c6afc68707efe19434
Navneet2025 <109101596+Navneet2025@users.noreply.github.com>
2022-09-25 22:17:41 +0530

    Time: 13 ms (25.31%), Space: 40.7 MB (65.64%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,21 +1,12 @@
 class Solution {
-    private int[] dp=new int[31];
-    public int fib(int n){
-        if(n<=1)
-            return n;
-        if(n==2)
+    public int fib(int n) {
+        if(n==0)
+            return 0;
+        if(n==1 || n==2)
             return 1;
-        memoize(n);
-        return dp[n];
-    }
-    public int memoize(int n) {
-       //top down approach
-        if(dp[n] !=0)
-            return dp[n];
-        if(n<3)
-            return 1;
-        else
-            dp[n]=memoize(n-1) + memoize(n-2);
-        return memoize(n);
+        if(n==3)
+            return 2;
+        
+        return fib(n-1) +fib(n-2);
     }
 }
