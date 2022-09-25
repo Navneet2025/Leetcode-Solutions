e5db86f28e34519e373db1837bd2aedb9ab49d65
Navneet2025 <109101596+Navneet2025@users.noreply.github.com>
2022-09-25 19:05:49 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (98.66%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,19 +1,18 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int count =0;
+        int  count=0;
         for(int i=s.length()-1;i>=0;i--){
-            
-        
-            if(s.charAt(i)!=' '){
+            if(s.charAt(i) != ' '){
                 count ++;
-                
-                
             }
+            
             else{
-                if(count > 0)
+                if(count>0){
                     return count;
+                }
             }
         }
+        
         return count;
     }
 }
