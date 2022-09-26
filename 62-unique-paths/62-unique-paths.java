4570a86ce5f7230e9bd8eed1aeecf95f2434e910
Navneet2025 <109101596+Navneet2025@users.noreply.github.com>
2022-09-26 09:19:43 +0530

    Time: 1 ms (56.22%), Space: 41 MB (50.09%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,6 +1,7 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m][n];
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 dp[i][j]=1;
@@ -8,8 +9,8 @@ class Solution {
         }
         
         for(int i=1;i<m;i++){
-            for(int  j=1;j<n;j++){
-                dp[i][j]=dp[i][j-1] +dp[i-1][j];
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i][j-1] + dp[i-1][j];
             }
         }
         
